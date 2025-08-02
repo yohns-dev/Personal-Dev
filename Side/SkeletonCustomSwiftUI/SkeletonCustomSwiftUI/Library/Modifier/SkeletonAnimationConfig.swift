4a1804fe74d805f2700b6115633c750d15de1361
고요한 <yohns@goyohan-ui-MacBookAir.local>
2025-08-02 14:31:43 +0900

    remove: remove SkeletonAnimationConfig defalut

--- Side/SkeletonCustomSwiftUI/SkeletonCustomSwiftUI/Library/Modifier/SkeletonAnimationConfig.swift
+++ Side/SkeletonCustomSwiftUI/SkeletonCustomSwiftUI/Library/Modifier/SkeletonAnimationConfig.swift
@@ -1,13 +1,7 @@
 
-struct SkeletonAnimationConfig: Equatable {
+struct SkeletonAnimationConfig {
     var duration: Double = 1.5
     var delay: Double = 0.0
     var autoreverses: Bool = true
-    
-    static let `default` = SkeletonAnimationConfig()
-    
-    var isCustomized: Bool {
-        self != .default
-    }
 }
 
