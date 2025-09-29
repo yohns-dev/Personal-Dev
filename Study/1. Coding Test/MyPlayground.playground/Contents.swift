4db8de029d980c9c0899946b1fee262414ae9c70
yohns-dev <yohns@goyohan-ui-MacBookAir.local>
2025-09-29 16:44:37 +0900

    Coding Test: kakao 가장 많이 받은 선물 다른 풀이

--- Study/1. Coding Test/MyPlayground.playground/Contents.swift
+++ Study/1. Coding Test/MyPlayground.playground/Contents.swift
@@ -58,6 +58,57 @@ func giftSolution_1(_ friends: [String], _ gifts: [String]) -> Int {
     return giftNextNumber.values.max() ?? 0
 }
 
-let result = giftSolution_1(["1", "2", "3"], ["1 2", "1 3", "1 3", "2 1", "3 3"])
-print(result)
+func giftSolution_2(_ friends: [String], _ gifts: [String]) -> Int {
+    var duplicationCountList = [String : Int]()
+    var reversalDuplicationList = [String : [Int]]()
+    var giftIndices = [String: Int]()
+    var giftNumberList = [String : Int]()
+    
+    //duplicationCountList
+    duplicationCountList = gifts.reduce(into: [:]) { $0[$1, default: 0] += 1 }
+    
+    for (key , value) in duplicationCountList {
+        let parts = key.split(separator: " ").map { String($0)}
+        guard parts.count == 2 else { continue }
+        
+        // giftIndices
+        giftIndices[parts[0], default: 0] += value
+        giftIndices[parts[1], default: 0] -= value
+        
+        //reversal
+        if reversalDuplicationList["\(parts[1]) \(parts[0])"] != nil { continue }
+        let otherValue = duplicationCountList["\(parts[1]) \(parts[0])"] ?? 0
+        reversalDuplicationList[key] = [value, otherValue]
+    }
+    
+    for i in 0..<friends.count {
+        for j in (i+1)..<friends.count {
+            let friend1 = friends[i]
+            let friend2 = friends[j]
+            
+            let friend12 = duplicationCountList["\(friend1) \(friend2)"] ?? 0
+            let friend21 = duplicationCountList["\(friend2) \(friend1)"] ?? 0
+            
+            if friend12 > friend21 {
+                giftNumberList[friend1, default: 0] += 1
+            } else if friend12 < friend21 {
+                giftNumberList[friend2, default: 0] += 1
+            } else {
+                let gift1 = giftIndices[friend1] ?? 0
+                let gift2 = giftIndices[friend2] ?? 0
+                if gift1 > gift2 {
+                    giftNumberList[friend1, default: 0] += 1
+                } else if gift1 < gift2 {
+                    giftNumberList[friend2, default: 0] += 1
+                }
+            }
+        }
+    }
+    
+    return giftNumberList.values.max() ?? 0
+}
 
+let result_1 = giftSolution_1(["muzi", "ryan", "frodo", "neo"], ["muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"])
+let result_2 = giftSolution_2(["muzi", "ryan", "frodo", "neo"], ["muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"])
+print(result_1)
+print(result_2)
