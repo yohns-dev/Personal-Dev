f0903746561a8d47913defe3bf6bc9773bfebf58
yohns-dev <yohns@goyohan-ui-MacBookAir.local>
2025-10-05 11:58:30 +0900

    Coding Test: kakao 프렌즈 4블록

--- Study/1. Coding Test/Kakao_Level_2.playground/Contents.swift
+++ Study/1. Coding Test/Kakao_Level_2.playground/Contents.swift
@@ -32,3 +32,62 @@ func newsClustingSolution(_ str1: String, _ str2: String) -> Int {
 }
 
 let result_1 = newsClustingSolution("handshake", "shake hands")
+
+
+//MARK: 프렌즈 4블록
+/// coordinatePlane[m][n]
+/// 나중에 remove나 리스트 부분을 수정해서 최소화 가능
+func friends4BlockSolution(_ m: Int, _ n: Int, _ board: [String]) -> Int {
+    var result: Int = 0
+    var coordinatePlane = board.map { Array($0) }
+    var disappearDict = [String : Bool]()
+    
+    while true {
+        for i in 0..<m-1 {
+            for j in 0..<n-1 {
+                if coordinatePlane[i][j] == " " { continue }
+                
+                if coordinatePlane[i][j] == coordinatePlane[i+1][j+1] && coordinatePlane[i][j] == coordinatePlane[i+1][j] && coordinatePlane[i][j] == coordinatePlane[i][j+1] {
+                    disappearDict["\(i),\(j)"] = true
+                    disappearDict["\(i+1),\(j)"] = true
+                    disappearDict["\(i),\(j+1)"] = true
+                    disappearDict["\(i+1),\(j+1)"] = true
+                }
+            }
+        }
+        
+        if disappearDict.count == 0 {
+            break
+        }
+        
+        
+        for i in disappearDict.keys {
+            let keyArray = i.split(separator: ",").map { Int($0)! }
+            coordinatePlane[keyArray[0]][keyArray[1]] = " "
+        }
+        
+        result += disappearDict.count
+        disappearDict.removeAll()
+        
+        for i in 0..<n {
+            var emptyList: [Int] = []
+            for j in stride(from: m-1, through: 0, by: -1) {
+                if coordinatePlane[j][i] == " " {
+                    emptyList.append(j)
+                    continue
+                }
+                else if coordinatePlane[j][i] != " " && !emptyList.isEmpty {
+                    coordinatePlane[emptyList.first ?? j][i] = coordinatePlane[j][i]
+                    coordinatePlane[j][i] = " "
+                    emptyList.append(j)
+                    emptyList.removeFirst()
+                }
+            }
+        }
+    }
+    
+    
+    return result
+}
+
+let result_2 = friends4BlockSolution(4,5, ["CCBDE", "AAADE", "AAABF", "CCBBF"])
